a80334a0a593977681760f2ee81e741bf2705b64
bryanathll <bryannatanael77@gmail.com>
2026-08-21 19:26:03 +0700

    feat: hapus FileMetaPlugin native, pratinjau PDF via PdfViewer in-app
    - hapus FileMetaPlugin.java & pendaftarannya di MainActivity.java
    - sinkron bundle www terbaru (PdfViewer pdf.js, tanpa jembatan native)

--- android/app/src/main/java/id/biz/juragansejati/portal/MainActivity.java
+++ android/app/src/main/java/id/biz/juragansejati/portal/MainActivity.java
@@ -32,10 +32,6 @@ public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Pendaftaran plugin harus dilakukan sebelum super.onCreate agar
-        // jembatan Capacitor mengenalinya saat WebView dibuat.
-        registerPlugin(FileMetaPlugin.class);
-
         super.onCreate(savedInstanceState);
 
         Window window = getWindow();
